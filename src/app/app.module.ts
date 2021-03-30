00c6b621616eac7e77324c9b396a83169b4346d3
sejalbanta29 <soodsejal5@gmail.com>
2021-03-30 22:55:51 +0530

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailComponent } from './detail/detail.component';
+import { ListingComponent } from './listing/listing.component';
+import { ObservableComponent } from './observable/observable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent,
+    ListingComponent,
+    ObservableComponent
   ],
   imports: [
     BrowserModule,
